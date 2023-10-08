d09180acbfaab5809861099fd3e6b9210adec19b
ZakharenkovaEkaterina <146060449+ZakharenkovaEkaterina@users.noreply.github.com>
2023-10-08 18:13:11 +0300

    2_4- Захаренкова

---
+++ ЛР1-2_4 исправленная.cs
@@ -0,0 +1,30 @@
+﻿using System;
+//2 уровень 4 задача
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double sum = 0;
+            double x = 0.9;
+            double last_sum = 0;
+            double term = 1;
+
+            for (int n = 1; n <= 10000; n++)
+            {
+                term *= x * x;
+                sum += term;
+
+                if (term >= 0.0001)
+                {
+                    last_sum = sum;
+                }
+                {
+                    Console.WriteLine(last_sum);
+                    break;
+                }
+            }
+        }
+    }
+}
