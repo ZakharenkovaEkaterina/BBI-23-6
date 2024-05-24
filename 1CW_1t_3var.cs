48aa85fb0bedd6299de2929e05bfdd7e06bfd938
ZakharenkovaEkaterina <146060449+ZakharenkovaEkaterina@users.noreply.github.com>
2024-05-24 15:30:25 +0300

    Zaharenkova_1CW

---
+++ 1CW_1t_3var.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+struct Vector //3 вариант номер 1
+{
+    public struct Dot
+    {
+        private int _x;
+        private int _y;
+        private int _z;
+
+        public Dot(int x, int y, int z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        public int X => _x;
+        public int Y => _y;
+        public int Z => _z;
+    }
+
+    private Dot _points1;
+    private Dot _points2;
+
+    public Vector(Dot point1, Dot point2)
+    {
+        _points1 = point1;
+        _points2 = point2;
+    }
+
+    public double Length()
+    {
+        double X = _points2.X - _points1.X;
+        double Y = _points2.Y - _points1.Y;
+        double Z = _points2.Z - _points1.Z;
+
+        return Math.Round(Math.Sqrt(X * X + Y * Y + Z * Z), 2);
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Координаты первой точки: ({_points1.X}, {_points1.Y}, {_points1.Z})");
+        Console.WriteLine($"Координаты второй точки: ({_points2.X}, {_points2.Y}, {_points2.Z})");
+        Console.WriteLine($"Длина вектора: {Length()}");
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Vector.Dot[] dots = new Vector.Dot[]
+        {
+            new Vector.Dot(4, 1, 2),
+            new Vector.Dot(1, 2, 7),
+            new Vector.Dot(7, 8, 9),
+            new Vector.Dot(11, 14, 13),
+            new Vector.Dot(13, 14, 15)
+        };
+
+        Vector[] vectors = new Vector[5];
+        vectors[0] = new Vector(dots[0], dots[1]);
+        vectors[1] = new Vector(dots[1], dots[2]);
+        vectors[2] = new Vector(dots[2], dots[3]);
+        vectors[3] = new Vector(dots[3], dots[4]);
+        vectors[4] = new Vector(dots[4], dots[0]);
+
+        for (int i = 0; i < vectors.Length - 1; i++)
+        {
+            for (int j = 0; j < vectors.Length - 1 - i; j++)
+            {
+                if (vectors[j].Length() < vectors[j + 1].Length())
+                {
+                    Vector t = vectors[j];
+                    vectors[j] = vectors[j + 1];
+                    vectors[j + 1] = t;
+                }
+            }
+        }
+
+        foreach (var vector in vectors)
+        {
+            vector.Print();
+        }
+    }
+}
---
+++ 1CW_2t_3var.cs
@@ -0,0 +1,172 @@
+﻿using System;
+abstract class Shape
+{
+    protected double _volume;
+    public double Volume => _volume;
+    public abstract void Calculate();
+    public abstract void Print1();
+}
+
+class Sphere : Shape
+{
+    protected double _radius;
+
+    public Sphere(double radius)
+    {
+        _radius = radius;
+        Calculate();
+    }
+
+    public override void Calculate()
+    {
+        _volume = (4.0 / 3.0) * Math.PI * Math.Pow(_radius, 3);
+        _volume = Math.Round(_volume, 2);
+    }
+
+    public override void Print1()
+    {
+        Console.WriteLine($"Сфера. Радиус: {_radius}, объем: {_volume}");
+    }
+}
+
+class Cube : Shape
+{
+    protected double _sideLength;
+
+    public Cube(double sideLength)
+    {
+        _sideLength = sideLength;
+        Calculate();
+    }
+
+    public override void Calculate()
+    {
+        _volume = Math.Pow(_sideLength, 3);
+        _volume = Math.Round(_volume, 2);
+    }
+
+    public override void Print1()
+    {
+        Console.WriteLine($"Куб. Длина стороны: {_sideLength}, объем: {_volume}");
+    }
+}
+
+class Cylinder : Shape
+{
+    protected double _radius;
+    protected double _height;
+
+    public Cylinder(double radius, double height)
+    {
+        _radius = radius;
+        _height = height;
+        Calculate();
+    }
+
+    public override void Calculate()
+    {
+        _volume = Math.PI * Math.Pow(_radius, 2) * _height;
+        _volume = Math.Round(_volume, 2);
+    }
+
+    public override void Print1()
+    {
+        Console.WriteLine($"Цилиндр. Радиус: {_radius} , высота: {_height}, объем: {_volume}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Shape[] sphere = new Shape[]
+        {
+      new Sphere(2),
+      new Sphere(4),
+      new Sphere(6),
+      new Sphere(1),
+      new Sphere(8)
+        };
+
+        Shape[] cube = new Shape[]
+        {
+      new Cube(6),
+      new Cube(8),
+      new Cube(2),
+      new Cube(5),
+      new Cube(4)
+        };
+
+        Shape[] cilinder = new Shape[]
+        {
+      new Cylinder(1, 4),
+      new Cylinder(2, 8),
+      new Cylinder(4, 9),
+      new Cylinder(3, 6),
+      new Cylinder(1, 7)
+        };
+
+        Sort(sphere);
+        Sort(cube);
+        Sort(cilinder);
+
+        Console.WriteLine("Сферы:");
+        Print2(sphere);
+
+        Console.WriteLine("Кубы:");
+        Print2(cube);
+
+        Console.WriteLine("Цилиндры:");
+        Print2(cilinder);
+
+        int totalShape = sphere.Length + cube.Length + cilinder.Length;
+        Shape[] allShape = new Shape[totalShape];
+
+        int index = 0;
+        foreach (var shape in sphere)
+        {
+            allShape[index] = shape;
+            index++;
+        }
+        foreach (var shape in cube)
+        {
+            allShape[index] = shape;
+            index++;
+        }
+        foreach (var shape in cilinder)
+        {
+            allShape[index] = shape;
+            index++;
+        }
+        Sort(allShape);
+
+        Console.WriteLine("Все фигуры:");
+        Print2(allShape);
+    }
+
+    static void Sort(Shape[] shapes)
+    {
+        int n = shapes.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (shapes[j].Volume < shapes[j + 1].Volume)
+                {
+                    Shape t = shapes[j];
+                    shapes[j] = shapes[j + 1];
+                    shapes[j + 1] = t;
+                }
+            }
+        }
+    }
+
+    static void Print2(Shape[] shapes)
+    {
+        foreach (var shape in shapes)
+        {
+            shape.Print1();
+        }
+        Console.WriteLine();
+    }
+}
